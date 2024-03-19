96c8c2e6aa76ca774723b165604785b0dcc2b1d2
Pickles <eldrago020907@gmail.com>
2024-03-19 13:47:44 -0400

    Main header
    Just added the main header and some comments

--- main.java
+++ main.java
@@ -1,3 +1,12 @@
 public class main {
-    
+
+    public static void main(String[] args) {
+
+        //should run signin class at this point
+
+        
+        //after this should be selection screen with GUI
+        //then launch option
+
+    }
 }
