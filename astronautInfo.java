1debb070cdea51e6078cbff7a9b9d92bafd7ebb5
MayKing25 <163891777+MayKing25@users.noreply.github.com>
2024-03-21 13:08:31 -0400

    Update and rename astronautInfo.java to ifWeDecidenotToUseGUI.java

--- astronautInfo.java
+++
@@ -1,3 +0,0 @@
-public class astronautInfo extends astronaut{
-  
-}
---
+++ ifWeDecidenotToUseGUI.java
@@ -0,0 +1,33 @@
+
+System.out.println("Welcome, Please put the astrnauts info below");
+// Use a do-while loop to display the options menu as follows until the user selects 3:
+           do{
+            // face of it and the potions
+            System.out.println("Menu:");
+            System.out.println("[1]Austronaut Info");
+            System.out.println("[2] Its time to launch");
+            // Enter your choice:
+            System.out.print("Enter your choice: ");
+            // Get the user's choice as an integer.
+            choice = kbd.nextInt();
+            // Based on the user's input, do the following:
+            switch(choice){
+                case 1:
+                // go to another switch statement that gives us the ability to edit th astronuts information
+                do{
+                System.out.printlnplease chose 
+                choicePt2 = kbd.nextInt();
+                switch(choicePt2){
+                  case 1:
+                }
+                  while(choicePt2 != )
+            break;
+                
+                case 2:
+                // who ever is doing the launch stuff add on to stuff here
+                break;
+                default:
+                // Display "Invalid choice. Please try again."
+                System.out.println("Invalid choice. Please try again.");
+                }
+           }while(choice != 2);
